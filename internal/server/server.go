3401bd0f231e0dcbb10ce83252369b0fc6e694ab
adongre <dongre.avinash@gmail.com>
2021-06-21 23:15:23 +0530

    Introduce cluster metadata

--- internal/server/server.go
+++ internal/server/server.go
@@ -10,6 +10,12 @@ import (
 	"syscall"
 )
 
+type MemberServer struct {
+	port        int
+	address     string
+	clusterName string
+}
+
 type Server struct {
 	name        string
 	port        int
@@ -17,6 +23,7 @@ type Server struct {
 	OSSignalCh  chan os.Signal
 	address     string
 	clusterName string
+	members     map[string]*MemberServer
 }
 
 func GetExistingServer() (*Server, error) {
@@ -30,6 +37,7 @@ func CreateNewServer(name string, address string, port int, clusterName string) 
 		address:     address,
 		port:        port,
 		clusterName: clusterName,
+		members:     map[string]*MemberServer{},
 	}
 	server.OSSignalCh = make(chan os.Signal, 1)
 	return &server, nil
@@ -38,6 +46,12 @@ func CreateNewServer(name string, address string, port int, clusterName string) 
 func (server *Server) Start() error {
 	signal.Notify(server.OSSignalCh, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
+	if member, ok := server.members[server.name]; ok {
+		if member.clusterName == server.clusterName {
+			return fmt.Errorf("Server with name %s already exists in cluster %s", server.name, server.clusterName)
+		}
+	}
+
 	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", server.address, server.port))
 	if err != nil {
 		return fmt.Errorf("failed to start server: %v", err)
@@ -45,10 +59,17 @@ func (server *Server) Start() error {
 	server.listener = listener
 
 	go func() {
-		http.Serve(server.listener, setupRouter(server))
+		err := http.Serve(server.listener, setupRouter(server))
+		if err != nil {
+			log.Fatalf("Failed to start a server, Error = %v", err)
+		}
 	}()
-
-	//SendMessage(member1, operations)
+	// Make your own entry into members map
+	server.members[server.name] = &MemberServer{
+		port:        server.port,
+		address:     server.address,
+		clusterName: server.clusterName,
+	}
 
 	for {
 		select {
@@ -57,7 +78,6 @@ func (server *Server) Start() error {
 			os.Exit(0)
 		}
 	}
-
 	return nil
 }
 
