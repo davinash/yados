a925f4147642aab122fdf7b2f6bd21498e64bc3e
adongre <dongre.avinash@gmail.com>
2021-07-12 15:46:20 +0530

    Adding test for unique name for each server in the cluster

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,16 +1,50 @@
 package server
 
-//func TestCreateNewServer(t *testing.T) {
-//	members, err := CreateClusterForTest(3)
-//	if err != nil {
-//		t.Log(err)
-//		t.FailNow()
-//	}
-//	defer func(cluster []Cluster) {
-//		err := StopTestCluster(cluster)
-//		if err != nil {
-//			t.Log(err)
-//			t.FailNow()
-//		}
-//	}(members)
-//}
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCreateNewServer(t *testing.T) {
+	members, err := CreateClusterForTest(3)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+	defer func(cluster []*YadosServer) {
+		err := StopTestCluster(cluster)
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
+	}(members)
+}
+
+func TestCreateNewServerDuplicateName(t *testing.T) {
+	cluster, err := CreateClusterForTest(3)
+	if err != nil {
+		t.Log(err)
+		t.FailNow()
+	}
+	defer func(cluster []*YadosServer) {
+		err := StopTestCluster(cluster)
+		if err != nil {
+			t.Log(err)
+			t.FailNow()
+		}
+	}(cluster)
+
+	srvDuplicate, err := startServerForTests("TestServer-0", "127.0.0.1", 9199)
+	if err != nil {
+		t.FailNow()
+	}
+	peers := make([]string, 0)
+	for _, p := range cluster {
+		peers = append(peers, fmt.Sprintf("%s:%d", p.self.Address, p.self.Port))
+	}
+	err = srvDuplicate.PostInit(peers)
+	if err == nil {
+		t.Log("startServerForTests should fail, duplicate name")
+		t.FailNow()
+	}
+}
