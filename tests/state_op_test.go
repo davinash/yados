7828be2d70ca591dc55140ccf2067066028d5628
adongre <dongre.avinash@gmail.com>
2021-08-23 12:48:19 +0530

    Adding the state information of raft to persistent

---
+++ tests/state_op_test.go
@@ -0,0 +1,36 @@
+package tests
+
+func (suite *YadosTestSuite) TestStateFileOp() {
+	WaitForLeaderElection(suite.cluster)
+
+	err := suite.cluster.members[0].PLog().WriteState(1, "abc")
+	if err != nil {
+		suite.T().Fatal(err)
+	}
+	term, votedFor, err := suite.cluster.members[0].PLog().ReadState()
+	if err != nil {
+		suite.T().Fatal(err)
+	}
+	if term != 1 {
+		suite.T().Fatalf("Failed to read term Expected 1, Actual %d", term)
+	}
+	if votedFor != "abc" {
+		suite.T().Fatalf("Failed to read term Expected abc, Actual %s", votedFor)
+	}
+
+	// write new state
+	err = suite.cluster.members[0].PLog().WriteState(2, "pqr")
+	if err != nil {
+		suite.T().Fatal(err)
+	}
+	term, votedFor, err = suite.cluster.members[0].PLog().ReadState()
+	if err != nil {
+		suite.T().Fatal(err)
+	}
+	if term != 2 {
+		suite.T().Fatalf("Failed to read term Expected 2, Actual %d", term)
+	}
+	if votedFor != "pqr" {
+		suite.T().Fatalf("Failed to read term Expected pqr, Actual %s", votedFor)
+	}
+}
