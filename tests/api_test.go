704eec8ab96e854fae62259f4d19b8221347d930
adongre <dongre.avinash@gmail.com>
2021-08-24 20:24:04 +0530

    disabling api test for now

--- tests/api_test.go
+++ tests/api_test.go
@@ -1,41 +1,31 @@
 package tests
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-
-	pb "github.com/davinash/yados/internal/proto/gen"
-)
-
-func (suite *YadosTestSuite) __TestAPI() {
-	WaitForLeaderElection(suite.cluster)
-
-	httpServer, port, err := AddNewServer(len(suite.cluster.members)+1, suite.cluster.members,
-		suite.logDir, "debug", true)
-	if err != nil {
-		suite.T().Fatal(err)
-	}
-	suite.cluster.members = append(suite.cluster.members, httpServer)
-
-	resp, err1 := http.Get(fmt.Sprintf("http://%s:%d/api/v1/status", httpServer.Self().Address, port))
-	if err1 != nil {
-		suite.T().Fatal(err1)
-	}
-	body, err2 := ioutil.ReadAll(resp.Body)
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-	defer resp.Body.Close()
-	sb := string(body)
-	var reply pb.ClusterStatusReply
-	err = json.Unmarshal([]byte(sb), &reply)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	suite.T().Log(sb)
-
-}
+//func (suite *YadosTestSuite) _TestAPI() {
+//	WaitForLeaderElection(suite.cluster)
+//
+//	httpServer, port, err := AddNewServer(len(suite.cluster.members)+1, suite.cluster.members,
+//		suite.logDir, "debug", true)
+//	if err != nil {
+//		suite.T().Fatal(err)
+//	}
+//	suite.cluster.members = append(suite.cluster.members, httpServer)
+//
+//	resp, err1 := http.Get(fmt.Sprintf("http://%s:%d/api/v1/status", httpServer.Self().Address, port))
+//	if err1 != nil {
+//		suite.T().Fatal(err1)
+//	}
+//	body, err2 := ioutil.ReadAll(resp.Body)
+//	if err2 != nil {
+//		log.Fatal(err2)
+//	}
+//	defer resp.Body.Close()
+//	sb := string(body)
+//	var reply pb.ClusterStatusReply
+//	err = json.Unmarshal([]byte(sb), &reply)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	suite.T().Log(sb)
+//
+//}
